347d2f1e60e231d2874d9913529c72339d0279ac
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-09-05 18:38:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const btn = document.getElementById("subt");
 console.log(btn);
 const guessed_num = document.getElementById("guessfield");
 console.log(guessed_num);
-const random_number = Math.floor(Math.random() * 100)+1;
-console.log(random_number);
+// const random_number = Math.floor(Math.random() * 100)+1;
+// console.log(random_number);
 
 btn.addEventListener("click", function(e){
+    const random_number = Math.floor(Math.random() * 100)+1;
+    console.log(random_number);
     const user_enter_num = guessed_num.value;
     console.log(isNaN(user_enter_num));
     if (isNaN(user_enter_num)){
@@ -17,10 +19,12 @@ btn.addEventListener("click", function(e){
     }else
     {
       if (random_number === user_enter_num){
-        document.getElementById("prev_guess").innerHTML = user_enter_num;
+        document.querySelectorAll("div > .guessing > p")[0].innerHTML = "The number is "+ random_number + " and your number is "+ user_enter_num;
+        document.querySelectorAll("div > .guessing > p")[1].innerHTML = "Congratulation you have guessed the number"
       } 
       else{
-        document.getElementById("prev_guess").innerHTML = user_enter_num;
+        document.querySelectorAll("div > .guessing > p")[0].innerHTML = "The number is "+ random_number + " and your number is "+ user_enter_num;
+        document.querySelectorAll("div > .guessing > p")[1].innerHTML = "oops, looks like you failed try again"
       } 
     }
 })
